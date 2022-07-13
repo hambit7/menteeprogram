32d0821b80ae6561665f3ce62e252f773617e0f0
Vitaliy Pashkov <vpashkov@cartera.com>
2022-07-13 19:05:57 +0300

    Working option

--- helper.php
+++ helper.php
@@ -13,17 +13,17 @@ function prettyPrint(array $inputData, $functionName , $left = false, $right = f
     echo ("<hr>");
     foreach ($inputData as $name => $data) {
         $_SESSION['iterationCounter'] = 0;
-        $result = $functionName($data, $left, $right);
+        $result = $functionName($data, 0,count($data)-1 , true);
 
         echo nl2br("Type of algoritm is : {$result['type']};\n");
         echo nl2br("Name of input array is : {$name};\n");
+//        echo print_r($result['data'], true);
         echo nl2br("Array size  is : {$result['dataCount']} elements;\n");;
         echo nl2br("Iterations count is : {$result['iterationsCount']} steps.\n");
         echo ("<hr>");
     }
     echo nl2br("end\n");
     echo "<br>";
-    return;
 }
 
 
